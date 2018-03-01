204f6a64b2d3ba73cc8e2020c7124d686536c1d9
theboymo <billfero@gmail.com>
2018-03-01 15:31:16 +0000

    implement custom layout file for devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  layout :layout_by_resource
 
   # redirect users to their profile page following successful sign in
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
+
+
+  private
+    def layout_by_resource
+      (devise_controller?)? "devise" : "application"
+    end
 end
