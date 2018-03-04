2d6a0c2a660fc9cbbb5d923d99c76a6364a69d73
theboymo <billfero@gmail.com>
2018-03-04 14:44:39 +0000

    refactor profile feature spec

--- spec/features/complete_profile.rb
+++ spec/features/complete_profile.rb
@@ -3,8 +3,7 @@ require 'capybara/rails'
 
 RSpec.feature 'Feature test: create user profile', type: :feature do
 
-
-	before {
+	scenario 'newly created user is directed to complete their profile' do
 		@user = FactoryBot.create(:user)
 		@user.confirmed_at = Time.now.utc
 		@user.save
@@ -12,15 +11,9 @@ RSpec.feature 'Feature test: create user profile', type: :feature do
 		fill_in 'user_email', with: "#{@user.email}"
 		fill_in 'user_password', with: "#{@user.password}"
 		click_button "Sign in"
-	}
 
-	it "directs user to their edit profile page" do
 		expect(current_path).to eq "/users/#{@user.id}/edit"
-	end
-
-	it "displays the user 'Edit your profile' page" do
 		expect(page).to have_content('Edit your profile')
 	end
-
 
 end
