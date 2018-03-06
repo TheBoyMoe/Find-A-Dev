af9dd986baa98e03820dcf516a5837ccaf2a17f0
theboymo <billfero@gmail.com>
2018-03-06 09:20:43 +0000

    implement add skill link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
 
-	def add_social_link_field(name, f, association)
+	def add_another_field(name, f, association)
 		new_object = f.object.send(association).klass.new # build a new answer instance
 		id = new_object.object_id # generate a unique 'id' for the object
 
 		# build a fields_for instance for the new answer, passing in a builder
 		fields = f.fields_for(association, new_object, child_index: id) do |builder|
-			# render the _answer_fields partial
+			# render the '[view]_fields partial
 			render(association.to_s.singularize + "_field", f: builder)
 		end
 		# generate a link to add more fields using js
