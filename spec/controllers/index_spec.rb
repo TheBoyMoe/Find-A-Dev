1772f62693f9ea8dda00a62651382db75c01ffd2
theboymo <billfero@gmail.com>
2018-03-07 20:31:46 +0000

    test index controller edit and update actions

---
+++ spec/controllers/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+	let(:developer) {FactoryBot.create(:user, name: 'Mock developer1', email: 'developer1@ex.com', role: 'developer', bio: "Ruby on Rails developer", confirmed_at: Time.now.utc)}
+	let(:founder) {FactoryBot.create(:user, name: 'Mock founder', email: 'founder@ex.com', role: 'founder', confirmed_at: Time.now.utc)}
+
+	describe "blocks a user from updating other user's profiles" do
+		before {
+			sign_in founder
+			patch :update,  params: {id: developer.id, bio: "Sales and marketing expert with over 10 years experience"}
+		}
+
+		it "redirects the user to the home page" do
+			expect(response).to redirect_to root_path
+		end
+
+		it "does not change developer bio" do
+			expect(developer.reload.bio).to eq "Ruby on Rails developer"
+		end
+	end
+
+
+	describe "a user is prevented from updating their profile with invalid data"
+
+end
