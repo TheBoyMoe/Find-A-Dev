130a8afe4cc34c52d81e27caaf1309cd48847167
theboymo <billfero@gmail.com>
2018-03-12 13:35:36 +0000

    add spec for the opportunity skill model

--- spec/models/opportunity_skill_spec.rb
+++ spec/models/opportunity_skill_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe OpportunitySkill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let!(:user) {FactoryBot.create(:user)}
+	let!(:opportunity) {FactoryBot.create(:opportunity, author: user)}
+
+	it "belongs to an opportunity" do
+		opportunity_skill = opportunity.opportunity_skills.create(title: 'Rails', description: 'Over six years experience')
+		expect(opportunity_skill.opportunity).to eq opportunity
+	end
+
 end
