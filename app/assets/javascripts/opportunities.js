8f76e1eb749238607ecce0aee51d802f757dc6de
theboymo <billfero@gmail.com>
2018-04-27 19:15:08 +0100

    use ajax to update the project title, description and skills

---
+++ app/assets/javascripts/opportunities.js
@@ -0,0 +1,42 @@
+
+$(function(){
+  $('#load-prev-project').on('click', (e)=>{
+    $.getJSON(e.target.href, (response)=>{
+      displayProject(response);
+    });
+    e.preventDefault();
+  });
+
+  function displayProject(response){
+   let opportunityContainer = document.querySelector('.opportunity-wrap');
+
+	 let opportunity =	
+     `<div class="opportunity">
+			  <h2>${response.title}</h2>
+        <p>${response.description}</p>
+		  </div>`;
+
+   if(response.opportunity_skills.length > 0){
+      opportunity += 
+				`<h3>You will have the following capabilities</h3>
+				<dl class="items skills">`;
+
+			response.opportunity_skills.forEach((skill)=>{
+				opportunity += 		
+        `<dt class="skill-title">${skill.title}</dt>
+				<dd class="skill-description">${skill.description}</dd>`
+      });
+			opportunity += `</dl></div>`;
+   }
+  
+
+		<div class="author-details">
+			<div class="image">
+				<%= image_tag user_avatar(@author.main_image), class: "circle" %>
+			</div>
+		</div>
+
+
+   opportunityContainer.innerHTML = opportunity;
+  }
+});
--- app/helpers/opportunity_helper.rb
+++ app/helpers/opportunity_helper.rb
@@ -3,7 +3,7 @@ module OpportunityHelper
   def prev_project_link(id)
     prev_id = id - 1;
     if(prev_id > 0 && prev_id <= Opportunity.count)
-      link_to 'Previous Project', opportunity_path(prev_id), class: "load-project"
+      link_to 'Previous Project', opportunity_path(prev_id), id: "load-prev-project"
     end
   end
 end
