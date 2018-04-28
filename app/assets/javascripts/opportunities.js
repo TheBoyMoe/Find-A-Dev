4309af1729ecad082d07f9aa9f5cdc71743804c0
theboymo <billfero@gmail.com>
2018-04-28 15:33:29 +0100

    add link to next project via js

--- app/assets/javascripts/opportunities.js
+++ app/assets/javascripts/opportunities.js
@@ -4,7 +4,6 @@ document.addEventListener('turbolinks:load', function() {
 
   function callback(e){
     e.preventDefault();
-    console.log('CLICKED NEXT');
 
     $.getJSON(e.target.href, (response)=>{
       displayProject(response);
@@ -12,33 +11,53 @@ document.addEventListener('turbolinks:load', function() {
   }
 
   function displayProject(response){
-   let opportunityContainer = document.querySelector('.opportunity-wrap');
+    let opportunityContainer = document.querySelector('.opportunity-wrap');
 
-	 let opportunity =	
-     `<div class="opportunity">
-			  <h2>${response.title}</h2>
-        <p>${response.description}</p>
-		  </div>`;
+	  let opportunity =	
+      `<div class="opportunity">
+	   	  <h2>${response.title}</h2>
+         <p>${response.description}</p>
+	     </div>`;
 
-   if(response.opportunity_skills.length > 0){
-      opportunity += 
-				`<h3>You will have the following capabilities</h3>
-				<dl class="items skills">`;
+    // include required skills
+    if(response.opportunity_skills.length > 0){
+       opportunity += 
+	   		`<h3>You will have the following capabilities</h3>
+	   		<dl class="items skills">`;
 
-			response.opportunity_skills.forEach((skill)=>{
-				opportunity += 		
-        `<dt class="skill-title">${skill.title}</dt>
-				<dd class="skill-description">${skill.description}</dd>`
-      });
-			opportunity += `</dl></div>`;
-   }
-  
+	   	response.opportunity_skills.forEach((skill)=>{
+	   		opportunity += 		
+         `<dt class="skill-title">${skill.title}</dt>
+	   		<dd class="skill-description">${skill.description}</dd>`
+       });
+	   	opportunity += `</dl></div>`;
+    }
+
+    // add user avatar
     opportunity += '<div class="author-details"><div class="image">';
-		opportunity += '<img src="'+ response.author.main_image.url + '"  class="circle" alt="profile image" >';
-		opportunity	+= '</div></div>';
+	  opportunity += '<img src="'+ response.author.main_image.url + '"  class="circle" alt="profile image" >';
+	  opportunity	+= '</div></div>';
+
+    // link to users profile
 
 
-   opportunityContainer.innerHTML = opportunity;
+    // start a conversation form
+
+
+    // link to next project
+    let elm = document.createElement('a');
+    if(response.id - 1 > 0){
+      let text = document.createTextNode('Next');
+      elm.appendChild(text);
+      elm.setAttribute('id', 'load-next-project');
+      elm.setAttribute('href', '/opportunities/' + (response.id - 1));
+      elm.addEventListener('click', callback);
+
+      // opportunity += '<a id="load-next-project" href="/opportunities/' + (response.id - 1) + '">Next</a>';
+    }
+
+    opportunityContainer.innerHTML = opportunity;
+    opportunityContainer.append(elm);
   }
 
 });
