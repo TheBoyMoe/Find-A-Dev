d046b0465aa349e90c0e19db21468d353e7d6a94
theboymo <billfero@gmail.com>
2018-04-29 15:31:59 +0100

    refactor messages index, move new message form to a partial and display it in the messages index view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,7 @@ class MessagesController < ApplicationController
 	end
 
 	def create
+    byebug
 		@message = @conversation.messages.build(message_params)
 		if @message.save
 			redirect_to conversation_messages_path(@conversation)
