82353f7310ba6d96b73e1e1bb54e7ef4f635ee8e
theboymo <billfero@gmail.com>
2018-04-30 15:06:53 +0100

    create a user list item in js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -20,7 +20,11 @@ document.addEventListener('turbolinks:load', function(){
   function displayResults(response){
     // check no of records returned
     if(response.length > 0){
-      console.log(response.log(response.length));
+      let htmlString = '';
+      response.forEach((user)=>{
+        htmlString += listItem(user);
+      });
+      console.log(htmlString);
     } else {
       let container = document.getElementById('container');
       let resultsList = document.getElementById('content-wrap');
@@ -40,8 +44,49 @@ document.addEventListener('turbolinks:load', function(){
 		    </div>
 	    </div>`;
 
-    // append to dom
     return alertMessage;
+  }
+
+  function listItem(user){
+	  return `<li class="item user">
+					<div class="contents-wrap clearfix">
+						<div class="profile">
+							<div class="image">
+								<img src="${user.main_image}" class="circle">
+							</div>
+							<div class="details">
+								<h3><a href="/users/${user.id}">${ user.name.upcase }</a></h3>
+								<p>${ user.email}</p>
+							  ${ displayLinks(user.social_links) }
+              </div>
+						</div>
+						<div class="contents">
+							<div class="bio-wrap">
+								<p class="bio">
+									<i class="fa fa-quote-left" aria-hidden="true"></i>
+									 ${ user.bio }
+									<i class="fa fa-quote-right" aria-hidden="true"></i>
+								</p>
+								<a href="/users/${user.id}">Read more</a>
+							</div>
+						</div>
+					</div>
+				</li>`;
+  }
+
+  function displayLinks(links){
+		let linkString = '';
+    if(links){
+				linkString += '<div>';
+				links.forEach((link)=>{
+					linkString += `<a href="${ link.url }" target="_blank" class="link_icon">
+						              <i class="fa fa-2x fa-${ link.name.downcase }"></i>
+					               </a>`;
+							
+        });
+				linkString +=	'</div>';
+     }
+     return linkString;
   }
 
 });
